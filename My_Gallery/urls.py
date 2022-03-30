d1a83350b0de761e773b65fe75442d2b2c83721c
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-03-30 14:44:28 +0300

    configure app urls

--- My_Gallery/urls.py
+++ My_Gallery/urls.py
@@ -3,3 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns=[
+    url(r'^$',views.pics,name='pics'),
+    url(r'^single_pic/(\d+)',views.single_pic),
+    url(r'^search/',views.search_results),
+    url(r'^location/(\d+)',views.viewPics_by_location),
+    url(r'^category/(\d+)',views.viewPics_by_category),
+    ]
