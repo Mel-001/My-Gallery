e1c11f91218603257896b5e240e1cf853e921933
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-03-30 14:48:02 +0300

    add admin.py

--- My_Gallery/admin.py
+++ My_Gallery/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import User,Category,Image,Location
 
 # Register your models here.
-
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Location)
