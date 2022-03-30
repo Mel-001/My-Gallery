08ab53b1853e7df1e690c33e9a70d44ed7700554
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-03-30 18:06:56 +0300

    admin page running

--- My_Gallery/urls.py
+++ My_Gallery/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns=[
     # url(r'^$',views.pics,name='pics'),
@@ -10,11 +11,12 @@ urlpatterns=[
     # url(r'^search/',views.search_results),
     # url(r'^location/(\d+)',views.viewPics_by_location),
     # url(r'^category/(\d+)',views.viewPics_by_category),
+    path('admin/', admin.site.urls),
     path('', views.pics, name='pics'),
     path('single_pic/', views.single_pic),
     path('search/', views.search_results),
     path('location/', views.viewPics_by_location),
-    path('category/', views.viewsPics_by_category)
+    path('category/', views.viewPics_by_category),
     ]
 
 
